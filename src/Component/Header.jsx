10515a7496a19dd0632e3be953a9e3f2ac94810b
sumit <sumitbagate3@gmail.com>
2025-02-13 16:15:17 +0530

    remove unused notification button from Header component

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -62,12 +62,7 @@ export default function Header() {
           </div>
 
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button
-              type="button"
-              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-none"
-            >
-              <BellIcon aria-hidden="true" className="size-6" />
-            </button>
+          
 
             {/* ✅ Profile dropdown */}
             <Menu as="div" className="relative ml-3">
